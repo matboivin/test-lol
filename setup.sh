afac50d9cfcb5e997ba6eaaf6775b6fcc3514821
mboivin <mboivin@student.42.fr>
2021-02-12 17:37:59 +0100

    Create namespace

--- setup.sh
+++ setup.sh
@@ -7,3 +7,10 @@ minikube start --vm-driver=docker
 
 # Enable dashboard
 minikube addons enable dashboard
+
+# Build images
+docker build -t nginx srcs/nginx
+docker build -t wordpress srcs/wordpress
+
+# Create namespace
+kubectl create -f ./srcs/ft_services.yaml
