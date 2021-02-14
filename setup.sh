0fc6c2304f7e448795c9c81bf113370de59f4c8d
mboivin <mboivin@student.42.fr>
2021-02-14 18:27:26 +0100

    Minor fixes

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@
 MINIKUBE_VERSION=v1.17.1
 KUBECTL_VERSION=v1.18.0
 IMAGES_TAG=1.0
-DOCKERFILE_PATH=docker-config
+DOCKERFILE_PATH=srcs/docker-config
 
 # Check directory exists
 if [ ! -d "/usr/local/bin/r" ]; then
@@ -13,9 +13,9 @@ if [ ! -d "/usr/local/bin/r" ]; then
 fi
 
 # Install minikube
-curl -Lo minikube https://storage.googleapis.com/minikube/releases/$MINIKUBE_VERSION/minikube-linux-amd64
-chmod +x minikube
-sudo install minikube /usr/local/bin/
+curl -Lo minikube https://storage.googleapis.com/minikube/releases/$MINIKUBE_VERSION/minikube-linux-amd64 \
+&& chmod +x minikube \
+&& sudo install minikube /usr/local/bin/
 
 if [ $? -eq 0 ]; then
     echo "√  minikube was installed"
@@ -27,8 +27,8 @@ fi
 
 # Install kubectl
 curl -LO https://storage.googleapis.com/kubernetes-release/release/$KUBECTL_VERSION/bin/linux/amd64/kubectl \
-&& chmod +x ./kubectl
-sudo mv ./kubectl /usr/local/bin/kubectl
+&& chmod +x ./kubectl \
+&& sudo mv ./kubectl /usr/local/bin/kubectl
 
 if [ $? -eq 0 ]; then
     echo "√  kubectl was installed"
