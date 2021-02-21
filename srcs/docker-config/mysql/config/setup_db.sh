3e4c1bc184561114297cbc52131f9328f5ff1f04
mboivin <mboivin@student.42.fr>
2021-02-21 19:47:19 +0100

    Wip mysql container

--- srcs/docker-config/mysql/config/setup_db.sh
+++ srcs/docker-config/mysql/config/setup_db.sh
@@ -2,20 +2,14 @@
 #
 # Setup database
 
-# Start service
-service mysql start
-
 # Create database user
-mysql -e "CREATE USER '$MYSQL_USER'@'localhost' IDENTIFIED BY '$MYSQL_PASSWORD'"
+mysql -e "CREATE USER '$MYSQL_USER'@'$HOSTNAME' IDENTIFIED BY '$MYSQL_PASSWORD'"
 
 # phpMyAdmin database
-mysql -e "GRANT ALL PRIVILEGES ON phpmyadmin.* TO '$MYSQL_USER'@'localhost';"
+mysql -e "GRANT ALL PRIVILEGES ON phpmyadmin.* TO '$MYSQL_USER'@'$HOSTNAME';"
 mysql -e "FLUSH PRIVILEGES;"
 
 # WordPress database
 mysql -e "CREATE DATABASE $MYSQL_DATABASE;"
-mysql -e "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'localhost';"
+mysql -e "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'$HOSTNAME';"
 mysql -e "FLUSH PRIVILEGES;"
-
-mysql $MYSQL_DATABASE -u root < /root/wordpress_db.sql
-rm /root/wordpress_db.sql
