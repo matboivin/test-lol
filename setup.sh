20e3eadfcd424f66b0c4dde905a3bcebf180af47
mboivin <mboivin@student.42.fr>
2021-02-21 01:33:31 +0100

    Minor fixes

--- setup.sh
+++ setup.sh
@@ -9,7 +9,7 @@ SCRIPTS_PATH=srcs/scripts
 echo "START FT_SERVICES\n"
 
 # INSTALL
-echo "STEP 1/4 Launch minikube\n"
+echo "Launch minikube\n"
 zsh $SCRIPTS_PATH/install_minikube.sh
 
 # Start cluster
@@ -26,11 +26,11 @@ minikube addons enable dashboard
 minikube addons enable metallb
 
 # DOCKER IMAGES
-echo "\nSTEP 2/4 Build docker images\n"
+echo "\nBuild docker images\n"
 zsh $SCRIPTS_PATH/build_docker.sh
 
 # CLUSTER
-echo "\nSTEP 3/4 Configure cluster\n"
+echo "\nConfigure cluster\n"
 kubectl apply -f $MANIFESTS_PATH --recursive
 
 #minikube dashboard
