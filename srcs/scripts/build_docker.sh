ad8da0be4b31e4048f3ae4804b0038076604b5bd
mboivin <mboivin@student.42.fr>
2021-02-22 03:41:29 +0100

    Build mysql image in setup.sh

--- srcs/scripts/build_docker.sh
+++ srcs/scripts/build_docker.sh
@@ -9,5 +9,8 @@ DOCKERFILE_PATH=srcs/docker-config
 docker build -t nginx:$IMAGES_TAG $DOCKERFILE_PATH/nginx
 echo "\n√   NGINX image was successfully built\n"
 
+docker build -t mysql:$IMAGES_TAG $DOCKERFILE_PATH/mysql
+echo "\n√   MySQL image was successfully built\n"
+
 docker build -t wordpress:$IMAGES_TAG $DOCKERFILE_PATH/wordpress
 echo "\n√   WordPress image was successfully built\n"
