f18179651fd258f7408c83701f219cfae0fdbde8
ouram <spiritslogs@protonmail.com>
2021-03-08 18:27:34 +0100

    Change scripts path

--- srcs/requirements/mysql/scripts/entrypoint.sh
+++ srcs/requirements/mysql/scripts/entrypoint.sh
@@ -5,7 +5,7 @@
 # Exit immediately if a command exits with a non-zero status
 set -e
 
-STARTUP_FILE=/root/startup.sh
+STARTUP_FILE=/root/scripts/startup.sh
 
 if [[ -f "$STARTUP_FILE" ]]; then
   chmod +x "$STARTUP_FILE" && ."$STARTUP_FILE"
