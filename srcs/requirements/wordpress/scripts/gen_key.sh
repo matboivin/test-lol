7562892c1a8eed94e637b90f8f7179235c36e2da
mboivin <mboivin@student.42.fr>
2021-03-12 17:18:28 +0100

    Update config

--- srcs/requirements/wordpress/scripts/gen_key.sh
+++ srcs/requirements/wordpress/scripts/gen_key.sh
@@ -2,15 +2,15 @@
 #
 # Generate a self-signed certificate and private key using OpenSSL
 
-CERT_PATH=/etc/ssl/certs/
 CERT_FILE=localhost.crt
 KEY_FILE=localhost.key
 
 apk update && apk add --no-cache openssl
-cd $CERT_PATH
+mkdir -p /etc/nginx/ssl
+cd /etc/nginx/ssl
 openssl req -x509 -sha256 -nodes -days 90 \
     -newkey rsa:2048 \
     -keyout $KEY_FILE \
     -out $CERT_FILE \
     -subj '/C=FR/CN=localhost' \
-&& chown 0600 $CERT_PATH/$KEY_FILE $CERT_PATH/$CERT_FILE
+&& chown 0600 /etc/nginx/ssl/$KEY_FILE /etc/nginx/ssl/$CERT_FILE
