5ca5d59c5842f4c241ac296c8fb084455114cefa
user42 <mboivin@student.42.fr>
2021-03-22 17:25:12 +0100

    Replace minikube ip in metallb config

--- setup.sh
+++ setup.sh
@@ -5,18 +5,20 @@
 # VARIABLES
 MANIFESTS_PATH=srcs/manifests
 SCRIPTS_PATH=srcs/scripts
-NAMESPACE_DEV=ft-services
+PROJECT_NAMESPACE=ft-services
 
-echo "START FT_SERVICES\n"
+echo "⧗   START FT_SERVICES\n"
 
 # INSTALL
-echo "Launch minikube\n"
+echo "⧗   Launch minikube\n"
 zsh $SCRIPTS_PATH/install_minikube.sh
+# Clean existing cluster
 minikube delete
 
 # Start cluster
 echo "⧗   Start the cluster ...\n"
 minikube start --driver=docker --kubernetes-version v1.20.2
+MINIKUBE_IP="$(minikube ip)"
 # Configure environment to use minikube’s Docker daemon
 eval $(minikube docker-env)
 # Check kubectl version
@@ -28,17 +30,22 @@ minikube addons enable dashboard
 minikube addons enable metallb
 
 # DOCKER IMAGES
-echo "\nBuild docker images\n"
+echo "\n⧗   Build docker images\n"
 zsh $SCRIPTS_PATH/build_docker.sh
 
-# CLUSTER
-echo "\nConfigure cluster\n"
+# CONFIGURE CLUSTER
+echo "⧗   Configure cluster\n"
+# Replace single IP in MetalLB config
+sed --in-place 's/singleip/'$MINIKUBE_IP'/g' $MANIFESTS_PATH/configmaps/metallb-config.yaml
+# Creates resources
 kubectl apply -f $MANIFESTS_PATH/00-namespace.yaml
 kubectl apply -f $MANIFESTS_PATH/secrets --recursive
 kubectl apply -f $MANIFESTS_PATH/configmaps --recursive
 kubectl apply -f $MANIFESTS_PATH/services --recursive
-
 # Set context to use ft-services as permanent namespace
-kubectl config set-context --current --namespace=$NAMESPACE_DEV
+kubectl config set-context --current --namespace=$PROJECT_NAMESPACE
+
+# PRINT INFORMATIONS
+echo "\n√   DONE\n\n    IP is: $MINIKUBE_IP"
 
 #minikube dashboard
