cc5925efba6f646df5ee4b3f606ef4ecbb12d1b6
user42 <mboivin@student.42.fr>
2021-03-23 01:28:31 +0100

    Change init script

--- srcs/requirements/mysql/scripts/init_db.sh
+++ srcs/requirements/mysql/scripts/init_db.sh
@@ -2,42 +2,33 @@
 #
 # Init database
 
-TMP_FILE=tmp_init.sql
+TMP_FILE=tmp_init
 
-if [ ! -d /var/lib/mysql/mysql ]; then
-	# Install MariaDB/MySQL in /var/lib/mysql
-	echo "⧗   Install MariaDB/MySQL system tables in '/var/lib/mysql' ..."
-	mysql_install_db --user=mysql --datadir=/var/lib/mysql > /dev/null
-	sleep 4
+if [ ! -d "/var/lib/mysql/mysql" ]; then
+  # Install MariaDB/MySQL in /var/lib/mysql
+  echo "⧗   Install MariaDB/MySQL system tables in '/var/lib/mysql' ..."
+  mysql_install_db --user=root --datadir=/var/lib/mysql > /dev/null
 
-	# Start mysqld in background
-	echo "⧗   Start mysqld in background ..."
-	mysqld_safe --datadir=/var/lib/mysql --pid-file=/run/mysqld/mysqld.pid &
-	sleep 2
+  if [ ! -d "/run/mysqld" ]; then
+    mkdir -p /run/mysqld
+  fi
 
-	# Create database
-	echo "⧗   Create database ..."
-
-	cat << EOF > $TMP_FILE
+  # Create database
+  echo "⧗   Create database ..."
+  cat << EOF > $TMP_FILE
 USE mysql;
 FLUSH PRIVILEGES;
 GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY "$MYSQL_ROOT_PASSWORD" WITH GRANT OPTION;
 GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost' WITH GRANT OPTION;
-ALTER USER 'root'@'localhost' IDENTIFIED BY '';
 
-CREATE DATABASE IF NOT EXISTS \`$MYSQL_DATABASE\`;
+CREATE DATABASE IF NOT EXISTS \`$MYSQL_DATABASE\` CHARACTER SET utf8 COLLATE utf8_general_ci;
 CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';
 GRANT ALL ON \`$MYSQL_DATABASE\`.* to '$MYSQL_USER'@'%' WITH GRANT OPTION;
-FLUSH PRIVILEGES;
 EOF
 
-	mysql -u root < $TMP_FILE
-	rm $TMP_FILE
-	# Kill mysqld
-	kill `cat /run/mysqld/mysqld.pid`
-	sleep 2
+  /usr/bin/mysqld --user=root --bootstrap --verbose=0 < $TMP_FILE
+  rm -f $TMP_FILE
 fi
 
 chown -R mysql:mysql /var/lib/mysql
-
 echo "√   Done"
