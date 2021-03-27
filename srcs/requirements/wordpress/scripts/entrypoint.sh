c31cd341af460992c257dd67b1a9869cabf60e8f
user42 <mboivin@student.42.fr>
2021-03-27 02:25:37 +0100

    WIP: wp-config.php mounted as configMap

--- srcs/requirements/wordpress/scripts/entrypoint.sh
+++ srcs/requirements/wordpress/scripts/entrypoint.sh
@@ -5,10 +5,4 @@
 # Exit immediately if a command exits with a non-zero status
 set -e
 
-sed --in-place 's/__DB_NAME__/'$WORDPRESS_DB_NAME'/g' /var/www/wordpress/wp-config.php
-sed --in-place 's/__DB_USER__/'$WORDPRESS_DB_USER'/g' /var/www/wordpress/wp-config.php
-sed --in-place 's/__DB_PASSWORD__/'$WORDPRESS_DB_PASSWORD'/g' /var/www/wordpress/wp-config.php
-sed --in-place 's/__DB_HOST__/'$WORDPRESS_DB_HOST'/g' /var/www/wordpress/wp-config.php
-sed --in-place 's/__DB_PORT__/'$WORDPRESS_DB_PORT'/g' /var/www/wordpress/wp-config.php
-
 exec "$@"
