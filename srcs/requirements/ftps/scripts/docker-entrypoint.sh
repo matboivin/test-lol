f72c72cbdf6e290fc427886ef370cb5e50149a51
user42 <mboivin@student.42.fr>
2021-04-04 01:10:59 +0200

    Simplify ftp dockerfile

--- srcs/requirements/ftps/scripts/docker-entrypoint.sh
+++ srcs/requirements/ftps/scripts/docker-entrypoint.sh
@@ -5,4 +5,20 @@
 # Exit immediately if a command exits with a non-zero status
 set -e
 
+# Create group and user
+addgroup --system $FTP_USER
+adduser \
+    --system \
+    --disabled-password \
+    --home /home/$FTP_USER \
+    --shell /sbin/nologin \
+    --ingroup $FTP_USER \
+    $FTP_USER
+echo "$FTP_USER:$FTP_PASS" | chpasswd
+
+# Secure chroot jail
+chown root:root /home/$FTP_USER
+mkdir -p /home/$FTP_USER/files
+chown $FTP_USER:$FTP_USER /home/$FTP_USER/files
+
 exec "$@"
