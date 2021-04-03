163b5febd2dd312d3f550a60999e844458fee893
user42 <mboivin@student.42.fr>
2021-04-03 18:38:51 +0200

    Fix gen_key script

--- srcs/requirements/ftps/scripts/gen_key.sh
+++ srcs/requirements/ftps/scripts/gen_key.sh
@@ -2,14 +2,10 @@
 #
 # Generate a self-signed certificate and private key using OpenSSL
 
-CERT_FILE=vsftpd.crt
-KEY_FILE=vsftpd.key
-
 apk update && apk add --no-cache openssl
-cd /etc/ssl/private
 openssl req -x509 -sha256 -nodes -days 90 \
     -newkey rsa:2048 \
-    -keyout $KEY_FILE \
-    -out $CERT_FILE \
-    -subj '/C=FR/CN=localhost' \
-&& chown 0600 /etc/ssl/private/$KEY_FILE /etc/ssl/private/$CERT_FILE
+    -keyout /etc/ssl/private/vsftpd-selfsigned.key \
+    -out /etc/ssl/certs/vsftpd-selfsigned.crt \
+    -subj '/C=FR/CN=ftps'
+chmod 0600 /etc/ssl/private/vsftpd-selfsigned.key /etc/ssl/certs/vsftpd-selfsigned.crt
--- srcs/requirements/wordpress/scripts/gen_key.sh
+++ srcs/requirements/wordpress/scripts/gen_key.sh
@@ -2,15 +2,10 @@
 #
 # Generate a self-signed certificate and private key using OpenSSL
 
-CERT_FILE=localhost.crt
-KEY_FILE=localhost.key
-
 apk update && apk add --no-cache openssl
-mkdir -p /etc/nginx/ssl
-cd /etc/nginx/ssl
 openssl req -x509 -sha256 -nodes -days 90 \
     -newkey rsa:2048 \
-    -keyout $KEY_FILE \
-    -out $CERT_FILE \
-    -subj '/C=FR/CN=localhost' \
-&& chown 0600 /etc/nginx/ssl/$KEY_FILE /etc/nginx/ssl/$CERT_FILE
+    -keyout /etc/ssl/private/nginx-selfsigned.key \
+    -out /etc/ssl/certs/nginx-selfsigned.crt \
+    -subj '/C=FR/CN=wordpress'
+chmod 0600 /etc/ssl/private/nginx-selfsigned.key /etc/ssl/certs/nginx-selfsigned.crt
