5903821d129e78f5a16cc9f5222d9f6a8a43f037
user42 <mboivin@student.42.fr>
2021-04-05 01:29:54 +0200

    Drop vsftpd cm

--- srcs/requirements/ftps/scripts/docker-entrypoint.sh
+++ srcs/requirements/ftps/scripts/docker-entrypoint.sh
@@ -5,6 +5,8 @@
 # Exit immediately if a command exits with a non-zero status
 set -e
 
+sed --in-place 's/__IP__/'$PASV_ADDRESS'/g' /etc/vsftpd/vsftpd.conf
+
 if ! id -u "$FTP_USER" > /dev/null 2>&1; then
     # Create FTP user
     adduser --disabled-password $FTP_USER
